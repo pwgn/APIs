878800314ee47996e29aaaba8a1c714444039855
Christoffer Persson <christoffer@fripost.org>
2017-03-17 11:26:23 +0100

    implement ratelimit decorator

--- Lesson_4/13_BargainMart/Starter Code/views.py
+++ Lesson_4/13_BargainMart/Starter Code/views.py
@@ -19,6 +19,8 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+redis = Redis()
+
 
 
 
@@ -29,7 +31,7 @@ class RateLimit(object):
 
     def __init__(self, key_prefix, limit, per, send_x_headers):
         self.reset = (int(time.time()) // per) * per + per
-        self.key = key_prefix + str(self.rest)
+        self.key = key_prefix + str(self.reset)
         self.limit = limit
         self.per = per
         self.send_x_headers = send_x_headers
@@ -41,7 +43,29 @@ class RateLimit(object):
     remaining = property(lambda x: x.limit - x.current)
     over_limit = property(lambda x: x.current >= x.limit)
 
+def get_view_rate_limit():
+    return getattr(g, '_view_rate_limit', None)
+
+def on_over_limit(limit):
+    return (jsonify({'data':'You hit the rate limit','error':'429'}),429)
+
+def ratelimit(limit, per=300, send_x_headers=True,
+                  over_limit=on_over_limit,
+                  scope_func=lambda: request.remote_addr,
+                  key_func=lambda: request.endpoint):
+    def decorator(f):
+        def rate_limited(*args, **kwargs):
+            key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
+            rlimit = RateLimit(key, limit, per, send_x_headers)
+            g._view_rate_limit = rlimit
+            if over_limit is not None and rlimit.over_limit:
+                return over_limit(rlimit)
+            return f(*args, **kwargs)
+        return update_wrapper(rate_limited, f)
+    return decorator
+
 @app.route('/catalog')
+@ratelimit(limit=10, per=60)
 def getCatalog():
     items = session.query(Item).all()
 
