c475b213a143ebb9347dadc9f754d170b9c5ebf2
Christoffer Persson <christoffer@fripost.org>
2017-03-17 10:20:03 +0100

    add ratelimit class

--- Lesson_4/13_BargainMart/Starter Code/views.py
+++ Lesson_4/13_BargainMart/Starter Code/views.py
@@ -2,7 +2,7 @@ from redis import Redis
 import time
 from functools import update_wrapper
 from flask import request, g
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 from models import Base, Item
 
 
@@ -24,8 +24,22 @@ app = Flask(__name__)
 
 app = Flask(__name__)
 #ADD RATE LIMITING CODE HERE
+class RateLimit(object):
+    expiration_window = 10
 
+    def __init__(self, key_prefix, limit, per, send_x_headers):
+        self.reset = (int(time.time()) // per) * per + per
+        self.key = key_prefix + str(self.rest)
+        self.limit = limit
+        self.per = per
+        self.send_x_headers = send_x_headers
+        p = redis.pipeline()
+        p.incr(self.key)
+        p.expireat(self.key, self.reset + self.expiration_window)
+        self.current = min(p.execute()[0], limit)
 
+    remaining = property(lambda x: x.limit - x.current)
+    over_limit = property(lambda x: x.current >= x.limit)
 
 @app.route('/catalog')
 def getCatalog():
@@ -51,7 +65,3 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
-     
-
-	
-   
